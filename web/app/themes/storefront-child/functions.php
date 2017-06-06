98cf5a0d9aa493c09463b564d965e21247b016c1
Jimmie Johansson <jimmitjoo@gmail.com>
2017-06-06 17:34:41 +0200

    add style and scripting

--- web/app/themes/storefront-child/functions.php
+++ web/app/themes/storefront-child/functions.php
@@ -1,4 +1,22 @@
 <?php
+add_action( 'wp_enqueue_scripts', 'yardson_enqueue_styles' );
+function yardson_enqueue_styles() {
+    wp_enqueue_style( 'storefront-style', get_template_directory_uri() . '/style.css' );
+}
+
+function yardson_enqueue_styles() {
+
+    $parent_style = 'storefront-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'yardson-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'yardson_enqueue_styles' );
+
 
 add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 12 );
 add_action( 'woocommerce_simple_add_to_cart', 'woocommerce_simple_add_to_cart', 12 );
