61b096f4bc8b74181764dd53435ceb23301eb943
Jimmie Johansson <jimmitjoo@gmail.com>
2017-06-06 17:37:01 +0200

    remove actions

--- web/app/themes/storefront-child/functions.php
+++ web/app/themes/storefront-child/functions.php
@@ -13,6 +13,13 @@ function yardson_enqueue_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'yardson_enqueue_styles' );
 
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+remove_action( 'woocommerce_simple_add_to_cart', 'woocommerce_simple_add_to_cart', 30 );
+remove_action( 'woocommerce_grouped_add_to_cart', 'woocommerce_grouped_add_to_cart', 30 );
+remove_action( 'woocommerce_variable_add_to_cart', 'woocommerce_variable_add_to_cart', 30 );
+remove_action( 'woocommerce_external_add_to_cart', 'woocommerce_external_add_to_cart', 30 );
+remove_action( 'woocommerce_single_variation', 'woocommerce_single_variation', 10 );
+remove_action( 'woocommerce_single_variation', 'woocommerce_single_variation_add_to_cart_button', 20 );
 
 add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 12 );
 add_action( 'woocommerce_simple_add_to_cart', 'woocommerce_simple_add_to_cart', 12 );
