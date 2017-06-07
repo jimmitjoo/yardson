6c586d5c6b01865a75e3069cf9935bf046a3bf9b
Jimmie Johansson <jimmitjoo@gmail.com>
2017-06-07 12:25:17 +0200

    cookie newsletter

--- web/app/themes/storefront-child/nyhetsbrev/js/nyhetsbrev.js
+++ web/app/themes/storefront-child/nyhetsbrev/js/nyhetsbrev.js
@@ -2,5 +2,6 @@ if ( get_cookie( 'has_visited' ) === 'undefined' ) {
     jQuery('.newsletter-popup').css({'display': 'block'});
     console.log('visa nyhetsbrev popup!');
 } else {
+    console.log(get_cookie( 'has_visited' ));
     console.log('visa inte nyhetsbrev popup!');
 }
