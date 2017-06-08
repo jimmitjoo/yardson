737c594459f43fe052a620539a467cdc05f206cc
Jimmie Johansson <jimmitjoo@gmail.com>
2017-06-08 19:27:40 +0200

    newsletter popup fix

--- web/app/themes/storefront-child/nyhetsbrev/js/nyhetsbrev.js
+++ web/app/themes/storefront-child/nyhetsbrev/js/nyhetsbrev.js
@@ -1,10 +1,12 @@
 if ( get_cookie( 'shown_create_account' ) === null ) {
     jQuery('document').ready(function() {
+        console.log('show nlpop');
         jQuery('#nlpop').slideUp();
         create_cookie('shown_create_account', 'yes', 21);
 
         jQuery('#nlpop .close').click(function(event) {
             jQuery('#nlpop').slideDown();
+            console.log('close nlpop');
         });
 
     });
